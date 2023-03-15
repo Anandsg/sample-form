94d1ce91f30efad151875c5c1eac6b35030167d3
Anandagadugin <gaduginanand984@gmail.com>
2023-03-15 12:02:35 +0530

    Creating Nodes and Modifying Dom

--- practice1.js
+++ practice1.js
@@ -1,34 +1,70 @@
-// Add item
-function addItem(e){
-    e.preventDefault();
-  
-    // Get input value
-    var newItem = document.getElementById('item').value;
-  
-    // Create new li element
-    var li = document.createElement('li');
-    // Add class
-    li.className = 'list-group-item';
-    // Add text node with input value
-    li.appendChild(document.createTextNode(newItem));
-  
-    // Create del button element
-    var deleteBtn = document.createElement('button');
-  
-    // Add classes to del button
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  
-    // Append text node
-    deleteBtn.appendChild(document.createTextNode('X'));
-  
-    // Append button to li
-    li.appendChild(deleteBtn);
-  
-    // Append li to list
-    itemList.appendChild(li);
-  
-    // Add event listener to delete button
-    deleteBtn.addEventListener('click', function(){
-      itemList.removeChild(li);
-    });
-  }
+// console.log(document.head);
+// console.log(document.domain);
+// console.log(document.images);
+// console.log(documnet.getElementById('header-title'));
+//
+
+// items[1].style.fontweight='bold';
+// items[1].style.backgroundcolor = 'green';
+
+//QUERY SELECTOR
+// var itemsList = document.getElementById("items");
+// var newItem = document.createElement("li");
+// newItem.textContent = "Item 5";
+// itemsList.appendChild(newItem);
+
+// var lastItem = document.querySelector('.list-group-item');
+// lastItem.style.color = 'green';
+
+// var secondItems = document.getElementById('items');
+// //var thirdItem = items.children[2];
+// thirdItem.style.display = 'none';
+
+//PARENTNODE
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
+
+//PARENT ELEMENT
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+
+//child node
+//console.log(itemList.childNodes);
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'Green';
+
+//First child
+console.log(itemList.firstChild);
+//first element child
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent  = 'Hellow!!';
+
+//last element child
+console.log(itemList.lastElement);
+itemList.lastElementChild.textContent = 'Hi';
+
+//next sibling
+console.log(itemList.nextSibling);
+//next element sibling
+console.log(itemList.nextElementSinling);
+
+//Previsou sibling
+console.log(itemList.previouSibling);
+//previous element sibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.backgroundColor = 'Yellow';
+
+//Create a element
+var newDiv =document.createElement('Div');
+//Create a div
+newDiv.className = 'hello';
+
+//Add id
+newDiv.id = 'hello1';
+
+//add attributes
+newDiv.setAttr ('title', 'hellow Div');
+console.log(newDiv);
